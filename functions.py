d5c8b12cf17e3c37e5f9f255cd41e2eb36ea7441
Max Proshkin <max.proshkin@mail.ru>
2024-04-11 01:30:56 +0300

    make function es_calculate(), remove some print's

---
+++ functions.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+K = 3.09
+M_P = 1.67262192 *10e-27 #кг
+E = 1.6 *10e-19
+T_E = 12 # 1.5 * 10e5
+EPS_0 = 8.85 * 10e-12
+
+def es_calculate(wires_count: int, 
+                 wires_length: float, 
+                 wires_radius: float, 
+                 wires_potential: float, 
+                 sc_mass: float, 
+                 sw_density: float, 
+                 sw_velocity:float) -> tuple[float]:
+    """Calculates unit force, force per wire, total force and
+        total acceleration by electric sail configuration."""
+        
+    r_0 = 2/E * np.sqrt((EPS_0 * T_E)/(sw_density))
+    under_exp = M_P * sw_velocity**2 / (E * wires_potential) * np.log(r_0 / wires_radius)
+    df_dz = K * M_P * sw_density * sw_velocity**2 * r_0 / (np.sqrt(np.exp(under_exp)) - 1)
+    
+    return df_dz, df_dz * wires_length, df_dz * wires_length * wires_count, df_dz * wires_length * wires_count / sc_mass
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from functions import es_calculate
+
 
 K = 3.09
 m_p = 1.67262192 *10e-27 #кг
@@ -9,27 +11,22 @@ e = 1.6 *10e-19
 T_e = 12 # 1.5 * 10e5
 eps_0 = 8.85 * 10e-12
 r_0 = 2/e * np.sqrt((eps_0 * T_e)/(n_0))
-print(r_0)
-
 
 
 def main():
     wires_cnt = int(input("Введите число проводов: "))
     wire_length = float(input("Введите длину провода(в километрах): "))
-    wire_radius = 1e-6 * float(input("Введите радиус провода(в милиметрах): "))
+    wire_radius = 1e-6 * float(input("Введите радиус провода(в микрометрах): "))
     wire_potential = 1000 * float(input("Введите потенциал провода(в кВ): "))
 
     mass = float(input("Введите массу корабля (в кг): "))
     
-    under_exp = m_p * V**2 / (e * wire_potential) * np.log(r_0 / wire_radius)
-
-    print(r_0)
-
-    df_dz = K * m_p * n_0 * V**2 * r_0 / (np.sqrt(np.exp(under_exp)) - 1)
+    df_dz, f_w, f_sc, ac_sc = es_calculate(wires_cnt, wire_length, wire_radius, wire_potential, mass, n_0, V)
+    
     print("Удельная сила действующая на провод:", df_dz)
-    print("Сила действующая на провод:", df_dz * wire_length)
-    print("Сила действующая на корабль:", df_dz * wire_length * wires_cnt)
-    print("Ускорение корабля:", df_dz * wire_length * wires_cnt / mass)
+    print("Сила действующая на провод:", f_w)
+    print("Сила действующая на корабль:", f_sc)
+    print("Ускорение корабля:", ac_sc)
 
 
 if __name__ == "__main__":
