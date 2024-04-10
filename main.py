868f9ec1bc479ef47014d638e800dcaab5fec881
Saakyan Mikhail <saakyan.mih@gmail.com>
2024-04-10 23:46:11 +0300

    fixed formulas

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import numpy as np
 
-K = 3.09 * 1e12
-M_P = 1.67262192e-27  # kg
-N_0 = 7.3 * 10e-15  # km^-3
-V = 3 * 1e2  # km/s
-R_0 = 0.02  # km
-E = 1.6e-19  # K
+
+K = 3.09
+m_p = 1.67262192 *10e-27 #кг
+n_0 = 7.3 #cm^-3
+V = 400
+e = 1.6 *10e-19
+T_e = 12 # 1.5 * 10e5
+eps_0 = 8.85 * 10e-12
+r_0 = 2/e * np.sqrt((eps_0 * T_e)/(n_0))
+print(r_0)
+
 
 
 def main():
     wires_cnt = int(input("Введите число проводов: "))
     wire_length = float(input("Введите длину провода(в километрах): "))
-    wire_radius = 1e-6 * float(input("Введите радиус провода(в миллиметрах): "))
+    wire_radius = 1e-6 * float(input("Введите радиус провода(в милиметрах): "))
     wire_potential = 1000 * float(input("Введите потенциал провода(в кВ): "))
 
     mass = float(input("Введите массу корабля (в кг): "))
+    
+    under_exp = m_p * V**2 / (e * wire_potential) * np.log(r_0 / wire_radius)
 
-    under_exp = M_P * V**2 / (E * wire_potential) * np.log(R_0 / wire_radius)
-    df_dz = 1000 * K * M_P * N_0 * V**2 * R_0 / (np.sqrt(np.exp(under_exp)) - 1)
+    print(r_0)
+
+    df_dz = K * m_p * n_0 * V**2 * r_0 / (np.sqrt(np.exp(under_exp)) - 1)
     print("Удельная сила действующая на провод:", df_dz)
     print("Сила действующая на провод:", df_dz * wire_length)
     print("Сила действующая на корабль:", df_dz * wire_length * wires_cnt)
